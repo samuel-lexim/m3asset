ec7a09c3edf48f4af0e87688427f04d48227d8ca
hiep <hiep.dao@leximit.com>
2023-01-16 21:43:53 +0700

    section grid box

--- wp-content/themes/m3asset/acf-layout/section-box_grid_section.php
+++ wp-content/themes/m3asset/acf-layout/section-box_grid_section.php
@@ -14,22 +14,22 @@ if (isset($args) && $args && $args['box_repeater'] && is_array($args['box_repeat
                     $headingId = $grid['box_item_heading'] ? createSlug($grid['box_item_heading']) : 'noID';
                     $heading = $grid['box_item_heading'] ?? '';
                     ?>
-                    <div id="<?= $headingId ?>" class="box_item" data-index="<?= $i ?>">
+                    <div id="<?= esc_attr($headingId) ?>" class="box_item" data-index="<?= esc_attr($i) ?>">
 
                         <?php if ($grid['box_item_image']) { ?>
                             <div class="_box_img">
-                                <img src="<?= $grid['box_item_image'] ?>" alt="<?= $heading ?>" />
+                                <img src="<?= esc_url($grid['box_item_image']) ?>" alt="<?= esc_attr($heading) ?>" />
                             </div>
                         <?php } ?>
 
 
                         <div class="_content">
                             <?php if ($heading) { ?>
-                                <h3 class="_box_item_heading darkBlue s20"><?= $heading ?></h3>
+                                <h3 class="_box_item_heading darkBlue s20"><?= esc_html($heading) ?></h3>
                             <?php } ?>
 
                             <?php if ($grid['box_item_summary']) { ?>
-                                <h4 class="_box_item_summary s16"><?= $grid['box_item_summary'] ?></h4>
+                                <h4 class="_box_item_summary s16"><?= esc_textarea($grid['box_item_summary']) ?></h4>
                             <?php } ?>
                         </div>
 
