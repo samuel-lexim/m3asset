b8aaeb8651876010b4ada16c705a1c3498a45992
hiep <hiep.dao@leximit.com>
2023-01-17 18:15:50 +0700

    Header

--- wp-content/themes/m3asset/functions.php
+++ wp-content/themes/m3asset/functions.php
@@ -21,30 +21,30 @@ if (!defined('_S_VERSION')) {
  */
 function m3asset_setup()
 {
-    /*
-        * Make theme available for translation.
-        * Translations can be filed in the /languages/ directory.
-        * If you're building a theme based on m3asset, use a find and replace
-        * to change 'm3asset' to the name of your theme in all the template files.
-        */
+    /**
+     * Make theme available for translation.
+     * Translations can be filed in the /languages/ directory.
+     * If you're building a theme based on m3asset, use a find and replace
+     * to change 'm3asset' to the name of your theme in all the template files.
+     */
     load_theme_textdomain('m3asset', get_template_directory() . '/languages');
 
     // Add default posts and comments RSS feed links to head.
     add_theme_support('automatic-feed-links');
 
-    /*
-        * Let WordPress manage the document title.
-        * By adding theme support, we declare that this theme does not use a
-        * hard-coded <title> tag in the document head, and expect WordPress to
-        * provide it for us.
-        */
+    /**
+     * Let WordPress manage the document title.
+     * By adding theme support, we declare that this theme does not use a
+     * hard-coded <title> tag in the document head, and expect WordPress to
+     * provide it for us.
+     */
     add_theme_support('title-tag');
 
-    /*
-        * Enable support for Post Thumbnails on posts and pages.
-        *
-        * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-        */
+
+    /**
+     * Enable support for Post Thumbnails on posts and pages.
+     * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+     */
     add_theme_support('post-thumbnails');
 
     // This theme uses wp_nav_menu() in one location.
@@ -54,10 +54,10 @@ function m3asset_setup()
         )
     );
 
-    /*
-        * Switch default core markup for search form, comment form, and comments
-        * to output valid HTML5.
-        */
+    /**
+     * Switch default core markup for search form, comment form, and comments
+     * to output valid HTML5.
+     */
     add_theme_support(
         'html5',
         array(
@@ -152,9 +152,9 @@ function m3asset_scripts()
     wp_enqueue_script('slick-1.8.1', get_template_directory_uri() . '/js/slick.min.js', array(), _S_VERSION, true);
     wp_enqueue_script('m3-script', get_template_directory_uri() . '/js/script.js', array(), _S_VERSION, true);
 
-    if (is_singular() && comments_open() && get_option('thread_comments')) {
-        wp_enqueue_script('comment-reply');
-    }
+//    if (is_singular() && comments_open() && get_option('thread_comments')) {
+//        wp_enqueue_script('comment-reply');
+//    }
 }
 
 add_action('wp_enqueue_scripts', 'm3asset_scripts');
@@ -177,7 +177,7 @@ require get_template_directory() . '/inc/template-functions.php';
 /**
  * Customizer additions.
  */
-require get_template_directory() . '/inc/customizer.php';
+//require get_template_directory() . '/inc/customizer.php';
 
 /**
  * Load Jetpack compatibility file.
@@ -188,6 +188,15 @@ if (defined('JETPACK__VERSION')) {
 
 
 // =========================== START CUSTOMIZE
+//add_theme_support('editor-gradient-presets', []);
+//add_theme_support('disable-custom-gradients', true);
+
+// Remove global-styles
+remove_action( 'wp_enqueue_scripts', 'wp_enqueue_global_styles' );
+remove_action( 'wp_footer', 'wp_enqueue_global_styles', 1 );
+
+// Remove SVG tag on body
+remove_action( 'wp_body_open', 'wp_global_styles_render_svg_filters' );
 
 /* Disable WordPress Admin Bar for all users */
 add_filter('show_admin_bar', '__return_false');
--- wp-content/themes/m3asset/js/script.js
+++ wp-content/themes/m3asset/js/script.js
@@ -1,48 +1,72 @@
-function svgInit() {
-    $('img[src$=".svg"]').each(function () {
-        let $img = $(this);
-        let imgID = $img.attr('id');
-        let imgClass = $img.attr('class');
-        let imgURL = $img.attr('src');
-        $.get(imgURL, function (data) {
-            // Get the SVG tag, ignore the rest
-            let $svg = $(data).find('svg');
-            // Get Class name
-            let $svgClass = $svg.attr('class') ? $svg.attr('class') : '';
-            // Add replaced image's ID to the new SVG
-            if (typeof imgID !== 'undefined') {
-                $svg = $svg.attr('id', imgID);
+(function ($) {
+
+    function svgInit() {
+        $('img[src$=".svg"]').each(function () {
+            let $img = $(this);
+            let imgID = $img.attr('id');
+            let imgClass = $img.attr('class');
+            let imgURL = $img.attr('src');
+            $.get(imgURL, function (data) {
+                // Get the SVG tag, ignore the rest
+                let $svg = $(data).find('svg');
+                // Get Class name
+                let $svgClass = $svg.attr('class') ? $svg.attr('class') : '';
+                // Add replaced image's ID to the new SVG
+                if (typeof imgID !== 'undefined') {
+                    $svg = $svg.attr('id', imgID);
+                }
+                // Add replaced image's classes to the new SVG
+                if (typeof imgClass !== 'undefined') {
+                    $svg = $svg.attr('class', imgClass + ' ' + $svgClass + ' replaced-svg');
+                }
+                // Remove any invalid XML tags as per http://validator.w3.org
+                $svg.removeAttr('xmlns:a');
+                // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+                if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                    $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'));
+                }
+                // Replace image with new SVG
+                $img.replaceWith($svg);
+            }, 'xml');
+        });
+    }
+
+    svgInit(); // Convert img to svg
+
+
+    $(document).ready(function () {
+        // Start - Header
+        $('#NavMenuButton').click(function (e, i) {
+            let parent = $(this).parent();
+
+            if (parent.hasClass('open')) {
+                $('.site-header').removeClass('open');
+                parent.removeClass('open');
+            } else {
+                $('.site-header').addClass('open');
+                parent.addClass('open');
             }
-            // Add replaced image's classes to the new SVG
-            if (typeof imgClass !== 'undefined') {
-                $svg = $svg.attr('class', imgClass + ' ' + $svgClass + ' replaced-svg');
+        });
+
+        $(document).keyup(function (e) {
+            // press esc
+            if (e.keyCode === 27) {
+                $('.nav-button-wrap').removeClass('open');
+                $('.site-header').removeClass('open');
             }
-            // Remove any invalid XML tags as per http://validator.w3.org
-            $svg.removeAttr('xmlns:a');
-            // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
-            if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'));
-            }
-            // Replace image with new SVG
-            $img.replaceWith($svg);
-        }, 'xml');
+        });
+        // End - Header
+
+        // Slider section - Slick
+        $('.slider_section_slick').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+            arrows: false,
+            fade: true,
+            adaptiveHeight: true
+        });
+
     });
-}
 
-svgInit(); // Convert img to svg
-
-
-
-$(document).ready(function () {
-
-    // Slider section - Slick
-    $('.slider_section_slick').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: true,
-        arrows: false,
-        fade: true,
-        adaptiveHeight: true
-    });
-
-});
+}(jQuery));
