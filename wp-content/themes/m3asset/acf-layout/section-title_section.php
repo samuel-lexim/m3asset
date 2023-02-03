7ee6d8e2cde6743e1b92e9efa3982208f5d218f8
hiep <hiep.dao@leximit.com>
2023-02-03 16:50:54 +0700

    m3invest - section title

--- wp-content/themes/m3asset/acf-layout/section-title_section.php
+++ wp-content/themes/m3asset/acf-layout/section-title_section.php
@@ -7,20 +7,25 @@ if ( isset( $args ) && $args ) {
     <div class="title_section">
         <?php
         $bg = $args['background'] ?? getDefaultImg();
+        $mobileBg = $args['mobile_background'] ?? getDefaultImg('default-hero-mobile.jpg');
         ?>
 
-        <div class="_inner" style="background-image: url(<?= esc_url($bg) ?>)">
-            <div class="_text_wrap lr_pad">
+        <div class="_bg_desktop" style="background-image: url(<?= esc_url($bg) ?>)"></div>
+
+        <div class="_bg_mobile">
+            <img src="<?= esc_url($mobileBg) ?>" alt="<?= esc_attr($args['the_title']) ?>" />
+        </div>
+
+        <div class="_text_wrap lr_pad">
             <?php if ( $args['the_title'] ) { ?>
                 <h1 class="_title <?= esc_attr($args['underline_color']) ?>"><?= esc_html($args['the_title']) ?></h1>
             <?php } ?>
 
             <?php if ( $args['description'] ) { ?>
-                <p class="_description"><?= esc_html($args["description"]) ?></p>
+                <div class="_description"><?= $args["description"] ?></div>
             <?php } ?>
-            </div>
-
         </div>
+
     </div>
 
 <?php }
